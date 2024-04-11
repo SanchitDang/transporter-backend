d71d05c9b383012c623a1659694b6a8240657d06
SanchitDang <work.sanchitdang@gmail.com>
2024-04-12 00:38:26 +0530

    Initial commit

---
+++ src/main/java/com/transporterbackend/TransporterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.transporterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporterBackendApplication.class, args);
+	}
+
+}
